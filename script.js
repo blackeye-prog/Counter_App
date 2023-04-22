d54b638e96d52f3fe22dd9bce2462fbca284dbbc
Nilesh Srivastav <80592367+blackeye-prog@users.noreply.github.com>
2023-04-22 13:20:14 +0530

    Upload files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const countValue = document.querySelector('#counter');
+
+const increment = () => {
+    //get the value
+    let value = parseInt(countValue.innerText);
+    //upate the value
+    value = value + 1;
+    //set the value
+    countValue.innerText=value;
+
+};
+
+const decrement = () => {
+    //get the value
+    let value = parseInt(countValue.innerText);
+    //upate the value
+    value = value - 1;
+    //set the value
+    countValue.innerText=value;
+};
